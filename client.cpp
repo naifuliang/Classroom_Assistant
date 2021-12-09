233575f5d5dfd1ef2fd19b5db00b40befbbddbe1
Ufowoqqqo <1085973120@qq.com>
2021-12-09 18:34:17 +0800

    Created Client class

--- client.cpp
+++ client.cpp
@@ -1,3 +1,13 @@
+// Author : Kaibin Lu (2021300004066)
+// Github @Ufowoqqqo
+
+// Organization :
+// Engineering Experimental Class,
+// Hongyi Honor College,
+// Wuhan University
+
+// Last updated : 2021/12/9
+
 #include "client.h"
 #include "ui_client.h"
 
@@ -38,10 +48,70 @@ void Client::disconnectSlot()
 
 void Client::newDataSlot()
 {
-    QByteArray _data;
+    QByteArray json;
+    QJsonDocument doc;
+    QJsonObject obj, content;
+    QStringList keys;
+    QString action, type, name, error;
+    bool is_successful;
 
-    _data = client->readAll();
-    qDebug() << _data;
+    json = client->readAll();
+    doc = QJsonDocument::fromJson(json);
+
+    if(doc.isObject())
+    {
+        obj = doc.object();
+        keys = obj.keys();
+
+        action = obj.value("act").toString();
+        content = obj.value("content").toObject();
+        is_successful = obj.value("is_successful").toBool();
+        type = obj.value("type").toString();
+        name = obj.value("name").toString();
+        error = obj.value("error").toString();
+        if(action == "register" && type == "student")
+        {
+            if(is_successful)
+                ; // successfully registered
+            else
+                ; // username existed
+        }
+        if(action == "register" && type == "teacher")
+        {
+            if(is_successful)
+                ; // successfully registered
+            else
+                ; // username existed
+        }
+        if(action == "login" && type == "student")
+        {
+            if(is_successful)
+                ; // successfully logined
+            else
+            {
+                if(error == "username_not_existed")
+                    ; // username not existed
+                if(error == "password_incorrect")
+                    ; // password incorrect
+                if(error == "already_online")
+                    ; // already online
+            }
+        }
+        if(action == "login" && type == "teacher")
+        {
+            if(is_successful)
+                ; // successfully logined
+            else
+            {
+                if(error == "username_not_existed")
+                    ; // username not existed
+                if(error == "password_incorrect")
+                    ; // password incorrect
+                if(error == "already_online")
+                    ; // already online
+            }
+        }
+    }
 
     return;
 }
@@ -63,6 +133,149 @@ void Client::_disconnect()
 void Client::_send(QByteArray text)
 {
     client->write(text);
+
+    return;
+}
+
+// When a teacher or a student attempts to register or login their account,
+// the JSON format from client to server should be as follows.
+
+/*
+ * [
+ *     {
+ *         "act" : "register" or "login"
+ *         "content" :
+ *         [
+ *             {
+ *                 "type" : "teacher" or "student"
+ *                 "name" :
+ *                 "password" :
+ *             }
+ *         ]
+ *     }
+ * ]
+ */
+
+// Meanwhile, the resulting JSON from server to client should be as follows.
+
+/*
+ * [
+ *     {
+ *         "act" : "register" or "login"
+ *         "content" :
+ *         [
+ *             {
+ *                 "is_successful" : true or false
+ *                 "type" : "teacher" or "student"
+ *                 "name" :
+ *                 "error" :
+ *                     "" (if successful) or
+ *                     "username_existed" (for register) or
+ *                     "username_not_existed" (for login) or
+ *                     "password_incorrect" (for login) or
+ *                     "already_online" (for login)
+ *             }
+ *         ]
+ *     }
+ * ]
+*/
+
+void Client::student_register(QByteArray username, QByteArray password)
+{
+    QByteArray hashPassword;
+    QJsonObject obj, subobj;
+    QString actiontypestr, usertypestr;
+    QByteArray json;
+    QJsonDocument doc;
+
+    hashPassword = QCryptographicHash::hash(password, QCryptographicHash::Md5);
+
+    subobj.insert("type", "student");
+    subobj.insert("name", QString(username));
+    subobj.insert("password", QString(hashPassword));
+
+    obj.insert("act", "register");
+    obj.insert("content", subobj);
+
+    doc = QJsonDocument(obj);
+    json = doc.toJson();
+
+    _send(json);
+
+    return;
+}
+
+void Client::teacher_register(QByteArray username, QByteArray password)
+{
+    QByteArray hashPassword;
+    QJsonObject obj, subobj;
+    QString actiontypestr, usertypestr;
+    QByteArray json;
+    QJsonDocument doc;
+
+    hashPassword = QCryptographicHash::hash(password, QCryptographicHash::Md5);
+
+    subobj.insert("type", "teacher");
+    subobj.insert("name", QString(username));
+    subobj.insert("password", QString(hashPassword));
+
+    obj.insert("act", "register");
+    obj.insert("content", subobj);
+
+    doc = QJsonDocument(obj);
+    json = doc.toJson();
+
+    _send(json);
+
+    return;
+}
+
+void Client::student_login(QByteArray username, QByteArray password)
+{
+    QByteArray hashPassword;
+    QJsonObject obj, subobj;
+    QString actiontypestr, usertypestr;
+    QByteArray json;
+    QJsonDocument doc;
+
+    hashPassword = QCryptographicHash::hash(password, QCryptographicHash::Md5);
+
+    subobj.insert("type", "student");
+    subobj.insert("name", QString(username));
+    subobj.insert("password", QString(hashPassword));
+
+    obj.insert("act", "login");
+    obj.insert("content", subobj);
+
+    doc = QJsonDocument(obj);
+    json = doc.toJson();
+
+    _send(json);
+
+    return;
+}
+
+void Client::teacher_login(QByteArray username, QByteArray password)
+{
+    QByteArray hashPassword;
+    QJsonObject obj, subobj;
+    QString actiontypestr, usertypestr;
+    QByteArray json;
+    QJsonDocument doc;
+
+    hashPassword = QCryptographicHash::hash(password, QCryptographicHash::Md5);
+
+    subobj.insert("type", "teacher");
+    subobj.insert("name", QString(username));
+    subobj.insert("password", QString(hashPassword));
+
+    obj.insert("act", "login");
+    obj.insert("content", subobj);
+
+    doc = QJsonDocument(obj);
+    json = doc.toJson();
+
+    _send(json);
 
     return;
 }
--- client.h
+++ client.h
@@ -1,9 +1,26 @@
+// Author : Kaibin Lu (2021300004066)
+// Github @Ufowoqqqo
+
+// Organization :
+// Engineering Experimental Class,
+// Hongyi Honor College,
+// Wuhan University
+
+// Last updated : 2021/12/9
+
 #ifndef CLIENT_H
 #define CLIENT_H
 
 #include <QObject>
 #include <QWidget>
 #include <QTcpSocket>
+#include <QJsonDocument>
+#include <QJsonArray>
+#include <QJsonObject>
+#include <QJsonValue>
+#include <QCryptographicHash>
+#include "login_student.h"
+#include "login_teacher.h"
 
 namespace Ui {
     class Client;
@@ -15,6 +32,11 @@ class Client : public QWidget
 public:
     explicit Client(QWidget *parent = nullptr);
     ~Client();
+
+    void student_register(QByteArray username, QByteArray password);
+    void teacher_register(QByteArray username, QByteArray password);
+    void student_login(QByteArray username, QByteArray password);
+    void teacher_login(QByteArray username, QByteArray password);
 
 signals:
     void _connect(QString _ip, quint16 _port);
